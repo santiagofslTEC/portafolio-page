c59c294c4588ab7e256d50e4cc1cf8b1a847c74f
Santiago Fernandez Somellera <a01646946@tec.mx>
2026-04-07 19:06:30 -0600

    Hero page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import OceanHero from "./components/OceanHero";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <OceanHero />
     </div>
   );
 }
